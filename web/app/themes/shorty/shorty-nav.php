fec63842157dbc1d946ed675ed243b71e182cd38
Afonso Duarte <afonsomartins@gmail.com>
2015-06-20 00:23:52 +0100

    remove stupid theme

--- web/app/themes/shorty/shorty-nav.php
+++
@@ -1,7 +0,0 @@
-              <div id="nav-container">
-              <p class="top-nav-position"><span><?php _e( 'Menu', 'shorty' ); ?></span><a href="#main_nav" id="access_nav" class="access_aid">Navigation</a></p>
-                  <nav id="main_nav">
-			<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-			<p class="top-aid-position"><a href="#body" id="access_top" class="access_aid">To Top</a></p>
-		</nav>
-              </div>
